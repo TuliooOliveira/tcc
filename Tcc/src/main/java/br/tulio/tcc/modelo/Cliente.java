6b7ad6507c4de6cc5019a79f82ff721aafd2e379
tulio.h.oliveira <tulio.h.oliveira@CPX-21DILU4P10R.dir.svc.accenture.com>
2018-04-22 19:35:04 -0300

    Dia 22.04

---
+++ Tcc/src/main/java/br/tulio/tcc/modelo/Cliente.java
@@ -0,0 +1,18 @@
+package br.tulio.tcc.modelo;
+
+public class Cliente extends Pessoa {
+	
+	private Integer codigo;
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+	
+
+}
