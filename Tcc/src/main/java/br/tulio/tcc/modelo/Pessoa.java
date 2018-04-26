478378b3a252778d0d352a132f5b444fcf2f7ea9
tulio.h.oliveira <tulio.h.oliveira@CPX-21DILU4P10R.dir.svc.accenture.com>
2018-04-25 23:55:28 -0300

    Commit dia 25/04

--- Tcc/src/main/java/br/tulio/tcc/modelo/Pessoa.java
+++ Tcc/src/main/java/br/tulio/tcc/modelo/Pessoa.java
@@ -1,37 +1,44 @@
 package br.tulio.tcc.modelo;
 
-import java.lang.reflect.Constructor;
 import java.util.Date;
 
 public abstract class Pessoa {
+	
 	private String nome;
 	private String cpf;
 	private Date data_nasc;
-	
-	
+	private String fone;
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public String getCpf() {
 		return cpf;
 	}
-	
+
 	public Date getData_nasc() {
 		return data_nasc;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	
+
 	public void setData_nasc(Date data_nasc) {
 		this.data_nasc = data_nasc;
 	}
 
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
 
 }
