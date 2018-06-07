1e9b878a06fbb92a74e1c5be0b56a281a167eaf2
tulio.h.oliveira <tulio.h.oliveira@CPX-21DILU4P10R.dir.svc.accenture.com>
2018-06-07 00:16:38 -0300

    Commit dia 06.06

--- Tcc/src/test/java/br/tulio/tcc/util/CidadeDAOTest.java
+++ Tcc/src/test/java/br/tulio/tcc/util/CidadeDAOTest.java
@@ -1,5 +1,8 @@
 package br.tulio.tcc.util;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.tulio.tcc.dao.CidadeDAO;
@@ -8,21 +11,36 @@ import br.tulio.tcc.entidade.Cidade;
 import br.tulio.tcc.entidade.Estado;
 
 public class CidadeDAOTest {
-	
+
 	@Test
+	@Ignore
 	public void salvar() {
 
-		EstadoDAO estadoDAO = new EstadoDAO();		
-		
+		EstadoDAO estadoDAO = new EstadoDAO();
+
 		Estado estado = estadoDAO.buscar(1);
-		
+
 		Cidade cidade = new Cidade();
 		cidade.setNome("Jaboatão dos Guararapes");
 		cidade.setEstado(estado);
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		cidadeDAO.salvar(cidade);
 
+	}
+	
+	@Test
+	public void listar() {
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		List<Cidade> resultado = cidadeDAO.listar();
+		
+		for (Cidade cidade : resultado) {
+			System.out.println("Código: " + cidade.getCodigo());
+			System.out.println("Nome da cidade: " + cidade.getNome());
+			System.out.println("Código do estado: " + cidade.getEstado().getCodigo());
+			System.out.println("Nome do estado: " + cidade.getEstado().getNome());
+			System.out.println("Sigla do estado: " + cidade.getEstado().getSigla());
+		}
 	}
 
 }
