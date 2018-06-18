14baf91a5bfed60f7dfcea7d379680974b7a3305
tulio.h.oliveira <tulio.h.oliveira@CPX-21DILU4P10R.dir.svc.accenture.com>
2018-06-17 23:21:38 -0300

    Commit dia 17.06

---
+++ Tcc/src/main/java/br/tulio/tcc/controller/EstadoController.java
@@ -0,0 +1,14 @@
+package br.tulio.tcc.controller;
+
+import javax.faces.bean.ManagedBean;
+
+/*@Named
+@RequestScoped*/
+@ManagedBean
+public class EstadoController {
+
+	public void salvar() {
+		
+		System.out.println("Teste BEAN");
+	}
+}
