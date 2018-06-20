4d24d1b5c0772363237acb7ba5f2fe2c9fe426bf
tulio.h.oliveira <tulio.h.oliveira@CPX-21DILU4P10R.dir.svc.accenture.com>
2018-06-19 22:19:50 -0300

    Commit dia 19.06

--- Tcc/src/main/java/br/tulio/tcc/controller/EstadoController.java
+++ Tcc/src/main/java/br/tulio/tcc/controller/EstadoController.java
@@ -1,7 +1,11 @@
 package br.tulio.tcc.controller;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
@@ -9,42 +13,90 @@ import br.tulio.tcc.dao.EstadoDAO;
 import br.tulio.tcc.entidade.Estado;
 
 //@Named	 // Bean CDI
-@ManagedBean //Bean JSF
-@ViewScoped // Tempo de vida dos objetos enquanto está na tela 
+@ManagedBean // Bean JSF
+@ViewScoped // Tempo de vida dos objetos enquanto está na tela
 public class EstadoController {
-	
+
 	private Estado estado;
-	
+	private List<Estado> estados;
+
 	public Estado getEstado() {
 		return estado;
 	}
-	
+
 	public void setEstado(Estado estado) {
 		this.estado = estado;
 	}
-	
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
+	@PostConstruct
+	public void listar() {
+
+		try {
+
+			EstadoDAO estadoDao = new EstadoDAO();
+			estados = estadoDao.listar();
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar lista os estados!");
+			erro.printStackTrace();
+		}
+
+	}
+
 	public void novo() {
 		estado = new Estado();
 	}
 
 	public void salvar() {
-		
+
 		try {
-			
+
 			EstadoDAO estadoDao = new EstadoDAO();
 			estadoDao.salvar(estado);
+
 			novo();
+			estados = estadoDao.listar(); // -> Atualiza tabela de listagem ao salvar novo estado
+
 			Messages.addGlobalInfo("Estado salvo com sucesso!");
-			
 		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o Estado!");
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o estado!");
+			erro.printStackTrace();
 		}
 
+		/*
+		 * String texto = "Teste message"; FacesMessage message = new
+		 * FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
+		 * 
+		 * FacesContext context = FacesContext.getCurrentInstance();
+		 * context.addMessage(null, message);
+		 */
+	}
 
-		/*String texto = "Teste message";
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
+	// Captura o item que foi selecionado da tabela da view
+	public void excluir(ActionEvent evento) {
+		
+		try {
 
-		FacesContext context = FacesContext.getCurrentInstance();
-		context.addMessage(null, message);*/
+			estado = (Estado) evento.getComponent().getAttributes().get("estadoSelecionado");
+			EstadoDAO estadoDao = new EstadoDAO();
+			estadoDao.excluir(estado);
+			
+			novo();
+			estados = estadoDao.listar(); // -> Atualiza tabela de listagem ao salvar novo estado
+
+			Messages.addGlobalInfo("Estado salvo com sucesso!");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar remover o estado!");
+			erro.printStackTrace();
+		}
+		
 	}
 }
